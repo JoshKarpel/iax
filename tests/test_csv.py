3f28ba2f94eb373b02ea080955f33681c951578c
JoshKarpel <josh.karpel@gmail.com>
2018-08-21 20:33:57 -0500

    cleanup

--- tests/test_csv.py
+++ tests/test_csv.py
@@ -7,32 +7,32 @@ from ix import ix
 
 def test_ints_to_csvs_with_header():
     @ix
-    class Ints:
+    class IntPair:
         a: int
         b: int
 
     ints = [
-        Ints(0, 1),
-        Ints(2, 5),
-        Ints(-10, -4),
+        IntPair(0, 1),
+        IntPair(2, 5),
+        IntPair(-10, -4),
     ]
 
     expected = "a,b\n0,1\n2,5\n-10,-4"
 
-    assert Ints.to_csvs(ints, write_header = True) == expected
+    assert IntPair.to_csvs(ints, write_header = True) == expected
 
 
 def test_ints_to_csvs_no_header():
     @ix
-    class Ints:
+    class IntPair:
         a: int
         b: int
 
     ints = [
-        Ints(0, 1),
-        Ints(2, 5),
-        Ints(-10, -4),
+        IntPair(0, 1),
+        IntPair(2, 5),
+        IntPair(-10, -4),
     ]
 
     expected = "0,1\n2,5\n-10,-4"
-    assert Ints.to_csvs(ints) == expected
+    assert IntPair.to_csvs(ints) == expected
